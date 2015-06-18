05aa7b443f7fbdc56f68d8e055b69a921b905784
rdg <rdg@rmcore.com>
2015-06-18 12:16:49 -0500

    Use transform translateX instead of basicLeft.

--- views/js/main.js
+++ views/js/main.js
@@ -496,7 +496,7 @@ function updatePositions() {
   }
 
   for (var i = 0; i < items.length; i++) {
-    items[i].style.left = items[i].basicLeft + 100 * phases[i % 5] + 'px';
+    items[i].style.transform = 'translateX(' + (100 * phases[i % 5] * 5) + 'px)';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
